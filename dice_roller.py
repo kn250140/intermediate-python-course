549cd2b8cef9aeb80176e0be2e6aff8f29763e33
kn250140 <69011978+kn250140@users.noreply.github.com>
2020-08-20 15:16:59 +0200

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -2,8 +2,8 @@ def main():
 import random
 dice_rolls = int(input('How many dice would you like to roll? '))
 dice_size = int(input('How many sides are the dice? '))
-dice_sum = 0
-for i in range(0,dice_rolls):
+dice_sum = 2
+for i in range(2,dice_rolls):
   roll = random.randint(1,dice_size)
   dice_sum += roll
   if roll == 1:
